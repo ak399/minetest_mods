e7ae2f5baa46c39bb3eb77460b4821e6aba2d3f1
ak399 <ak399g@gmail.com>
2013-09-08 19:54:24 -0400

    fix wield_image

--- ithildin-ak399/init.lua
+++ ithildin-ak399/init.lua
@@ -68,6 +68,42 @@ minetest.register_node("ithildin:ithildin_stone_1", {
 	furnace_burntime = 4,
 })
 
+--ithildin_glowstone node
+
+minetest.register_node("ithildin:ithildin_glowstone_0", {
+	description = "Ithildin Glowstone",
+	tiles = {"ithildin_glowstone_0.png"},
+--	inventory_image = {"ithildin_glowstone_1.png"},
+--	wield_image = {"ithildin_glowstone_1.png"},
+	paramtype2 = "facedir",
+	drawtype = 'normal',
+	walkable = true,
+	pointable = true,
+	sunlight_propagates = false,
+	drop = "ithildin:ithildin_glowstone_1",
+	groups = {snappy=2,cracky=3},
+	sounds = default.node_sound_glass_defaults(),
+	furnace_burntime = 4,
+})
+
+minetest.register_node("ithildin:ithildin_glowstone_1", {
+	description = "Ithildin Glowstone",
+	tiles = {"ithildin_glowstone_1.png"},
+--	inventory_image = ("ithildin_glowstone_1.png"),
+--	wield_image = ("ithildin_glowstone_1.png"),
+	paramtype2 = "facedir",
+	drawtype = 'glasslike',
+	walkable = true,
+	pointable = true,
+	sunlight_propagates = true,
+	light_source = 8,
+	drop = "ithildin:ithildin_glowstone_1",
+	groups = {snappy=2,cracky=3},
+	sounds = default.node_sound_glass_defaults(),
+	furnace_burntime = 4,
+})
+
+
 --craft
 
 minetest.register_craft({
@@ -83,7 +119,7 @@ minetest.register_craft({
 	output = "ithildin:ithildin_stone_1",
 	recipe = {
 	{"default:torch", "moreores:mithril_ingot", "default:torch"},
-	{"default:glass", "default:stone", "default:glass"},
+	{"default:glass", "default:cobble", "default:glass"},
 	{"default:torch", "moreores:mithril_ingot", "default:torch"},
 	},
 })
@@ -92,7 +128,7 @@ minetest.register_craft({
 	output = "ithildin:ithildin_stone_1",
 	recipe = {
 	{"ithildin:ithildin_1"},
-	{"default:stone"},
+	{"default:cobble"},
 	},
 })
 
@@ -105,9 +141,26 @@ minetest.register_craft({
 	},
 	replacements = {
 	{"ithildin:ithildin_stone_1", ""},
-	{"", "default:stone"},
+	{"", "default:cobble"},
 	},
 })]]
+
+minetest.register_craft({
+	output = "ithildin:ithildin_glowstone_1",
+	recipe = {
+	{"default:torch", "moreores:mithril_ingot", "default:torch"},
+	{"default:glass", "default:stone", "default:glass"},
+	{"default:torch", "moreores:mithril_ingot", "default:torch"},
+	},
+})
+
+minetest.register_craft({
+	output = "ithildin:ithildin_glowstone_1",
+	recipe = {
+	{"ithildin:ithildin_1"},
+	{"default:stone"},
+	},
+})
 
 --ithildin function
 
@@ -157,6 +210,32 @@ minetest.register_abm(
 		if minetest.env:get_timeofday() < 0.2 or minetest.env:get_timeofday() > 0.8
 		then
 			minetest.env:set_node(pos, {name="ithildin:ithildin_stone_1"})
+		end
+	end,
+})
+
+--ithildin_glowstone function
+
+minetest.register_abm(
+	{nodenames = {"ithildin:ithildin_glowstone_1"},
+	interval = 1,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		if minetest.env:get_timeofday() > 0.2 and minetest.env:get_timeofday() < 0.8
+		then
+			minetest.env:set_node(pos, {name="ithildin:ithildin_glowstone_0"})
+		end
+	end,
+})
+
+minetest.register_abm(
+	{nodenames = {"ithildin:ithildin_glowstone_0"},
+	interval = 1,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		if minetest.env:get_timeofday() < 0.2 or minetest.env:get_timeofday() > 0.8
+		then
+			minetest.env:set_node(pos, {name="ithildin:ithildin_glowstone_1"})
 		end
 	end,
 })
